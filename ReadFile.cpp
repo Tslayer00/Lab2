2a69bdfd8239f25ade134cc2d5031cdd5f7fa121
Travis Dixon <tmdixon42@students.tntech.edu>
2016-09-01 14:34:10 -0500

    your comments for the commit

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -1,4 +1,4 @@
-#include "ReadFile.h"
+#include "ReadFile_class.h"
 #include <iostream>
 #include <string>
 
---
+++ ReadFile_class.h
@@ -0,0 +1,24 @@
+#if !defined READ_FILE
+#define READ_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class ReadFile
+{
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	public:
+		ReadFile(const char* file_name);
+		~ReadFile();
+		String(ReadFile* rf);
+		~String();
+	
+};
+
+
+#endif
